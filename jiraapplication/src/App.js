7422c24b5a06cb74eca78f92b91c0cab6c3aad29
cemkulunkoglu <62657442+cemkulunkoglu@users.noreply.github.com>
2024-08-26 11:43:31 +0300

    commit

--- jiraapplication/src/App.js
+++ jiraapplication/src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
 import TaskCreate from './components/TaskCreate';
 import TaskList from './components/TaskList';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Toast, ToastContainer } from 'react-bootstrap';
+import axios from 'axios';
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
 
-  const createTask = (title, taskDesc) => {
+  const createTask = async (title, taskDesc) => {
+    const newId = tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 1;
+
+    const response = await axios.post('http://localhost:3004/tasks', {
+      id: newId,
+      title,
+      taskDesc,
+    });
+
     const createdTasks = [
-      ...tasks, {
-        id: Math.round(Math.random() * 999999),
-        title,
-        taskDesc
-      }
+      ...tasks,
+      response.data
     ];
 
     setTasks(createdTasks);
@@ -23,17 +29,42 @@ function App() {
     setShowToast(true);
   };
 
-  const deleteTaskById = (id) => {
-    const afterDeletingTasks = tasks.filter((task) => {
-      return task.id !== id;
-    });
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get('http://localhost:3004/tasks');
+        setTasks(response.data);
+      } catch (error) {
+        console.error("Görevler yüklenirken hata oluştu", error);
+      }
+    };
 
+    fetchTasks();
+  }, []);
+
+  useEffect(() => {
+    if (tasks.length > 0) {
+      setShowToast(true);
+    }
+  }, [tasks]);
+
+  const deleteTaskById = async (id) => {
+    console.log(id); // ID'nin doğru olup olmadığını kontrol edin
+    await axios.delete(`http://localhost:3004/tasks/${id}`);
+    const afterDeletingTasks = tasks.filter((task) => task.id !== id);
+  
     setTasks(afterDeletingTasks);
     setToastMessage("Görev başarıyla silindi!");
-    setShowToast(true);
   };
+  
 
-  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put(`http://localhost:3004/tasks/${id}`, {
+      id,
+      title: updatedTitle,
+      taskDesc: updatedTaskDesc
+    });
+
     const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
@@ -43,7 +74,6 @@ function App() {
 
     setTasks(updatedTasks);
     setToastMessage("Görev başarıyla güncellendi!");
-    setShowToast(true);
   };
 
   return (
@@ -51,7 +81,7 @@ function App() {
       <TaskCreate onCreate={createTask} />
       <hr />
       <TaskList tasks={tasks} onDelete={deleteTaskById} onUpdate={editTaskById} />
-      
+
       <ToastContainer position="bottom-end" className="p-3">
         <Toast onClose={() => setShowToast(false)} show={showToast} delay={3000} autohide>
           <Toast.Header>
